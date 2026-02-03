084e65309a1d468bf818d6dc9f1ec9f18e133001
Femi Precious <femioye09@gmail.com>
2026-02-03 15:10:50 +0100

    Added Stats and Who We Are page UI

---
+++ frontend/components/Stats.tsx
@@ -0,0 +1,23 @@
+import { div, label } from "motion/react-client";
+import Stat from "./Stat";
+type arrayStat = {
+  label:string,
+  users:string
+}
+const statArray: arrayStat[]= [{users:"500+",label: "Active Members"},
+  {users:"120+",label:"Events Anually"},
+  {users:"15",label:"Technical Societies"},
+  {users:"Global",label: "Active Members"}
+]
+function Stats() {
+  return (
+    <div className="flex flex-wrap justify-center gap-6 items-center py-10">
+      {statArray.map((item, index) => {
+        return (
+          <Stat key={index} label={item.label} users={item.users} />
+        )
+      })}
+    </div>
+  )
+}
+export default Stats;
